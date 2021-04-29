0b2f86d92184cab9760a99ff179c236d7e430752
bogdandonduk <bogdandonduk@gmail.com>
2021-04-29 17:44:25 +0700

    Commit 7

--- permissionsandroid/src/main/java/bogdandonduk/androidlibs/permissionsandroid/PermissionRequester.kt
+++ permissionsandroid/src/main/java/bogdandonduk/androidlibs/permissionsandroid/PermissionRequester.kt
@@ -4,5 +4,5 @@ import android.view.View
 import androidx.fragment.app.DialogFragment
 
 class PermissionRequester {
-    val rationaleModalsMap = mutableMapOf<String, Pair<(view: View, modal: DialogFragment) -> Unit, (view: View, modal: DialogFragment) -> Unit>>()
+    val rationaleModalsMap = mutableMapOf<String, Pair<() -> Unit, () -> Unit>>()
 }
