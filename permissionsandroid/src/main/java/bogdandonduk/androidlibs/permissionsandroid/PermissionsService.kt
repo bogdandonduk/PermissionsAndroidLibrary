cfe5370a6cb2a41d9202111f49d2783aa27c6e52
bogdandonduk <bogdandonduk@gmail.com>
2021-04-30 16:45:25 +0700

    Commit 18

--- permissionsandroid/src/main/java/bogdandonduk/androidlibs/permissionsandroid/PermissionsService.kt
+++ permissionsandroid/src/main/java/bogdandonduk/androidlibs/permissionsandroid/PermissionsService.kt
@@ -97,6 +97,14 @@ object PermissionsService {
             alreadyGrantedOrLessThanApi23Action.invoke()
     }
 
+    fun requestStorageUnwrapped(activity: Activity) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+                openAppSettingsForManageStorage(activity)
+            else
+                activity.requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), codesMap[STORAGE]!!)
+    }
+
     @SuppressLint("NewApi")
     fun handleStorageRequestResult(activity: Activity, requestCode: Int, grantResults: IntArray, grantedAction: (() -> Unit)? = null, deniedAction: (() -> Unit)? = null) {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
